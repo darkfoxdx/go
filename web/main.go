71073a2debd2805837270efdba8ea6ca674ac9a3
Eugene Low <eugene.ws.low@gmail.com>
2024-06-08 17:07:01 +0800

    Go Web example

---
+++ web/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/books/{title}/page/{page}", func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		title := vars["title"] // the book title slug
+		page := vars["page"]   // the page
+
+		fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
+		fmt.Fprintf(w, "You've requested the book: %s on page %s\n", title, page)
+
+	})
+
+	fs := http.FileServer(http.Dir("static/"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	http.ListenAndServe(":80", r)
+}
