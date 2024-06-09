f736c3f87a6a8582c80f180fb80b4b623c770e6e
Eugene Low <eugene.ws.low@gmail.com>
2024-06-09 21:42:46 +0800

    Web

--- web/main.go
+++ web/main.go
@@ -2,26 +2,45 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
+type Todo struct {
+	Title string
+	Done  bool
+}
+
+type TodoPageData struct {
+	PageTitle string
+	Todos     []Todo
+}
+
 func main() {
+	tmpl := template.Must(template.ParseFiles("layout.html"))
+
 	r := mux.NewRouter()
 
-	r.HandleFunc("/books/{title}/page/{page}", func(w http.ResponseWriter, r *http.Request) {
+	r.HandleFunc("/{title}", func(w http.ResponseWriter, r *http.Request) {
 		vars := mux.Vars(r)
-		title := vars["title"] // the book title slug
-		page := vars["page"]   // the page
+		title := vars["title"] // the title
 
-		fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
-		fmt.Fprintf(w, "You've requested the book: %s on page %s\n", title, page)
+		// fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
+		// fmt.Fprintf(w, "You've requested the book: %s on page %s\n", title, page)
+
+		data := TodoPageData{
+			PageTitle: fmt.Sprintf("Todo title: %s", title),
+			Todos: []Todo{
+				{Title: "Task 1", Done: false},
+				{Title: "Task 2", Done: true},
+				{Title: "Task 3", Done: true},
+			},
+		}
+		tmpl.Execute(w, data)
 
 	})
-
-	fs := http.FileServer(http.Dir("static/"))
-	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
 	http.ListenAndServe(":80", r)
 }
