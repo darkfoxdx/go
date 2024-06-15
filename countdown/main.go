06ce6723c29577312bb3cf43ff4bed7200c68de1
Eugene Low <eugene.ws.low@gmail.com>
2024-06-15 23:10:39 +0800

    Countdown part 2

--- countdown/main.go
+++ countdown/main.go
@@ -1,28 +1,151 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"slices"
+)
+
+type Node struct {
+	indice      []int
+	calculation []int
+	calc_string string
+	total       int
+}
 
 func main() {
 	//	target := 8
-	num := []int{2, 3, 6}
+	num := []int{2, 3, 6, 8}
 	eq := []int{0, 1, 2} //0 = +, 1 = -, 2 =
-	//calc := [][]int{}
+	calc := []Node{}
 
 	//	found := false
-	for i := 0; i < len(num)-1; i++ {
-		for j := 0; j < len(eq); j++ {
-			var result int
-			switch j {
-			case 0:
-				result = num[i] + num[i+1]
-			case 1:
-				result = num[i] - num[i+1]
-			case 2:
-				result = num[i] * num[i+1]
+	for i := 0; i < len(num); i++ {
+		result := Node{
+			indice:      []int{i},
+			calculation: []int{num[i]},
+			calc_string: fmt.Sprintf("%d", num[i]),
+			total:       num[i],
+		}
+		calc = append(calc, result)
+	}
+
+	length := len(calc)
+
+	for i := 0; i < length; i++ {
+		selected := calc[i]
+		for j := 0; j < len(num); j++ {
+			if !slices.Contains(selected.indice, j) {
+				for e := 0; e < len(eq); e++ {
+					var result Node
+					switch eq[e] {
+					case 0:
+						result = Node{
+							indice:      append(selected.indice, j),
+							calculation: append(selected.calculation, e, num[j]),
+							calc_string: fmt.Sprintf("%s+%d", selected.calc_string, num[j]),
+							total:       selected.total + num[j],
+						}
+					case 1:
+						result = Node{
+							indice:      append(selected.indice, j),
+							calculation: append(selected.calculation, e, num[j]),
+							calc_string: fmt.Sprintf("%s-%d", selected.calc_string, num[j]),
+							total:       selected.total - num[j],
+						}
+					case 2:
+						result = Node{
+							indice:      append(selected.indice, j),
+							calculation: append(selected.calculation, e, num[j]),
+							calc_string: fmt.Sprintf("%s*%d", selected.calc_string, num[j]),
+							total:       selected.total * num[j],
+						}
+					}
+
+					calc = append(calc, result)
+				}
 			}
-			// calc[i*j+j] = []int{num[i], j, num[i+1], result}
-			fmt.Printf("%d %d, %d, %d, %d\n", i*len(num)+j, num[i], j, num[i+1], result)
 		}
 	}
 
+	oldlength := length
+	length = len(calc)
+
+	for i := oldlength; i < length; i++ {
+		selected := calc[i]
+		for j := 0; j < len(num); j++ {
+			if !slices.Contains(selected.indice, j) {
+				for e := 0; e < len(eq); e++ {
+					var result Node
+					switch eq[e] {
+					case 0:
+						result = Node{
+							indice:      append(selected.indice, j),
+							calculation: append(selected.calculation, e, num[j]),
+							calc_string: fmt.Sprintf("%s+%d", selected.calc_string, num[j]),
+							total:       selected.total + num[j],
+						}
+					case 1:
+						result = Node{
+							indice:      append(selected.indice, j),
+							calculation: append(selected.calculation, e, num[j]),
+							calc_string: fmt.Sprintf("%s-%d", selected.calc_string, num[j]),
+							total:       selected.total - num[j],
+						}
+					case 2:
+						result = Node{
+							indice:      append(selected.indice, j),
+							calculation: append(selected.calculation, e, num[j]),
+							calc_string: fmt.Sprintf("%s*%d", selected.calc_string, num[j]),
+							total:       selected.total * num[j],
+						}
+					}
+
+					calc = append(calc, result)
+				}
+			}
+		}
+	}
+
+	oldlength = length
+	length = len(calc)
+
+	for i := oldlength; i < length; i++ {
+		selected := calc[i]
+		for j := 0; j < len(num); j++ {
+			if !slices.Contains(selected.indice, j) {
+				for e := 0; e < len(eq); e++ {
+					var result Node
+					switch eq[e] {
+					case 0:
+						result = Node{
+							indice:      append(selected.indice, j),
+							calculation: append(selected.calculation, e, num[j]),
+							calc_string: fmt.Sprintf("%s+%d", selected.calc_string, num[j]),
+							total:       selected.total + num[j],
+						}
+					case 1:
+						result = Node{
+							indice:      append(selected.indice, j),
+							calculation: append(selected.calculation, e, num[j]),
+							calc_string: fmt.Sprintf("%s-%d", selected.calc_string, num[j]),
+							total:       selected.total - num[j],
+						}
+					case 2:
+						result = Node{
+							indice:      append(selected.indice, j),
+							calculation: append(selected.calculation, e, num[j]),
+							calc_string: fmt.Sprintf("%s*%d", selected.calc_string, num[j]),
+							total:       selected.total * num[j],
+						}
+					}
+
+					calc = append(calc, result)
+				}
+			}
+		}
+	}
+
+	for i, e := range calc {
+		fmt.Printf("%d %s=%d\n", i, e.calc_string, e.total)
+	}
 }
